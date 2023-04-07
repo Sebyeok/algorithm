b5ed2e1cb0e6175ec5daebb4a6465c5548c82ab4
Sebyeok <35682237+Sebyeok@users.noreply.github.com>
2023-04-07 18:06:58 +0900

    [level 2] Title: 타겟 넘버, Time: 1.57 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,18 @@
 from collections import deque
-import math
 
 def solution(numbers, target):
     answer = 0
-    queue = deque([numbers[0],-numbers[0]])
-    for i in range(1, len(numbers)):
-        for j in range(int(math.pow(2,i))):
-            v = queue.popleft()
-            queue.append(v+numbers[i])
-            queue.append(v-numbers[i])
-    for num in queue:
+    queue1 = deque([numbers[0], -numbers[0]])
+    queue2 = deque()
+    for number in numbers[1:]:
+        while(queue1):
+            v = queue1.popleft()
+            queue2.append(v+number)
+            queue2.append(v-number)
+        queue1 = queue2.copy()
+        queue2.clear()
+    
+    for num in queue1:
         if(num==target):
             answer+=1
     return answer
