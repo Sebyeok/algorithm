4c4c3b481bc2070c16c6a5c239b9e4bea17c19ac
Sebyeok <35682237+Sebyeok@users.noreply.github.com>
2023-04-07 18:13:52 +0900

    Update 타겟 넘버.py

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -16,3 +16,16 @@ def solution(numbers, target):
         if(num==target):
             answer+=1
     return answer
+
+def solution2(numbers, target):
+    answer = 0
+    queue = deque([numbers[0],-numbers[0]])
+    for i in range(1, len(numbers)):
+        for j in range(int(math.pow(2,i))):
+            v = queue.popleft()
+            queue.append(v+numbers[i])
+            queue.append(v-numbers[i])
+    for num in queue:
+        if(num==target):
+            answer+=1
+    return answer
